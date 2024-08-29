2262396db1fd9291ae5b0066d5988b848faa7ec9
magnum15 <108329693+magnum15@users.noreply.github.com>
2024-08-29 12:54:17 -0600

    Fonts colors change

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,27 +1,26 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultTheme = require('tailwindcss/defaultTheme');
 
 function rem2px(input, fontSize = 16) {
     if (input == null) {
-        return input
+        return input;
     }
     switch (typeof input) {
         case 'object':
             if (Array.isArray(input)) {
-                return input.map((val) => rem2px(val, fontSize))
+                return input.map((val) => rem2px(val, fontSize));
             }
-            // eslint-disable-next-line no-case-declarations
-            const ret = {}
+            const ret = {};
             for (const key in input) {
-                ret[key] = rem2px(input[key], fontSize)
+                ret[key] = rem2px(input[key], fontSize);
             }
-            return ret
+            return ret;
         case 'string':
-            return input.replace(/(\d*\.?\d+)rem$/, (_, val) => `${parseFloat(val) * fontSize}px`)
+            return input.replace(/(\d*\.?\d+)rem$/, (_, val) => `${parseFloat(val) * fontSize}px`);
         case 'function':
-            return eval(input.toString().replace(/(\d*\.?\d+)rem/g, (_, val) => `${parseFloat(val) * fontSize}px`))
+            return eval(input.toString().replace(/(\d*\.?\d+)rem/g, (_, val) => `${parseFloat(val) * fontSize}px`));
         default:
-            return input
+            return input;
     }
 }
 
@@ -34,19 +33,37 @@ module.exports = {
             keyframes: {
                 'fade-in': {
                     '0%': {
-                        opacity: '0'
+                        opacity: '0',
                     },
                     '100%': {
-                        opacity: '1'
-                    }
-                }
+                        opacity: '1',
+                    },
+                },
             },
             animation: {
-                'fade-in': 'fade-in 0.3s ease-out'
-            }
-        }
+                'fade-in': 'fade-in 0.3s ease-out',
+            },
+            typography: {
+                DEFAULT: {
+                    css: {
+                        // Aplicar color a los encabezados h1 a h6
+                        'h1, h2, h3, h4, h5, h6': {
+                            color: '#cab286',  // Cambia al color deseado
+                        },
+                        // Aplicar color a los números de las listas ordenadas
+                        'ol > li::marker': {
+                            color: '#cab286',  // Cambia al color deseado
+                        },
+                        // Aplicar color a los elementos <strong>
+                        'strong': {
+                            color: '#cab286',  // Cambia al color deseado
+                        },
+                    },
+                },
+            },
+        },
     },
     plugins: [
-      require('@tailwindcss/typography')
-    ]
-}
+        require('@tailwindcss/typography'),
+    ],
+};
